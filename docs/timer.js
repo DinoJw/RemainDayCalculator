80e26287b27bd04ac6493d53786bd509196b364d
DinoJw <l99044655@gmail.com>
2022-10-10 23:59:42 +0900

    home page update

--- docs/timer.js
+++ docs/timer.js
@@ -1,7 +1,11 @@
-function init() {
+let innerDiv;
+let container;
+function Init() {
+	container = document.querySelector("#container")
+	innerDiv = container.children;
 	let now = new Date();
 	let isEvent = false;
-	let TextBox = document.querySelector("#container div:nth-child(2)");
+	let TextBox = innerDiv[1];
 	let eventDay, eventName;
 	function setTextBox(str){
 		TextBox.textContent = str;
@@ -16,10 +20,10 @@ function init() {
 			if (event[0] == e) {
 				eventDay = new Date(event[1]);
 				eventName = event[2];
-				return;
+				return true;
 			}
 		}
-		isEvent = false;
+		return false
 	}
 	function getTimeLeft() {
 		curTime = new Date();
@@ -52,25 +56,76 @@ function init() {
 				break;
 		}
 	})
+	if (parameters.length == 1 && !isEvent) { InitHome(); return }
 	if (eventName == undefined) { eventName = "일정";parameters.push([]);}
 
+	document.title = eventName + '까지 남은 시간'
+	if (parameters.length < 3 && !isEvent) {
+		GoToHome();
+		return;
+	}
+
 	if(isEvent) {
-		eventFunc(event);
+		if(!eventFunc(event)) {
+			GoToHome();
+			return;
+		}
 	}else{
 		eventMD = '-'+month+'-'+day;
 		eventDay = new Date(new Date().getFullYear() + eventMD);
 	}
-	document.title = eventName + '까지 남은 시간'
-	if (parameters.length < 3 && !isEvent) {
-		setTextBox('매개변수가 충분하지 않습니다.');
-		return;
-	}
+
 	if(eventDay.getTime() < now.getTime()) {
 		setTextBox(eventName + "은(는) 이미 시작되었거나 지났습니다.");
 		return;
 	}
 
-	document.querySelector("#container div:first-child").textContent = eventName + "까지";
-	document.querySelector("#container div:last-child").textContent = "남았습니다";
+	innerDiv[0].textContent = eventName + "까지";
+	innerDiv[2].textContent = "남았습니다";
 	setInterval(getTimeLeft, 500);
+}
+
+function GoToHome() {
+	location.href = "./index.html";
+}
+
+function InitHome() {
+	innerDiv[1].textContent = "";
+	document.title = "일정 설정하기";
+	document.body.id = "home";
+	const DAYS_IN_MONTH = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+	let title = document.querySelector("title");
+	let result = document.querySelector("#urlCopy")
+	let body = document.body;
+	let url = location.href;
+	let inputs = [];
+	for (let div of innerDiv) {
+		let temp = document.createElement("input");
+		inputs.push(temp);
+		div.appendChild(temp);
+	}
+	inputs[0].placeholder = "일정의 이름";
+	inputs[1].placeholder = "월";
+	inputs[2].placeholder = "일";
+
+	inputs[1].addEventListener("focus", () => { innerDiv[1].classList.remove("error") });
+	inputs[2].addEventListener("focus", () => { innerDiv[2].classList.remove("error") });
+	result.addEventListener("click", () => {
+		let error = false;
+		let name = inputs[0].value;
+		let month = inputs[1].value;
+		let day = inputs[2].value;
+
+		if (month < 1 || month > 12 || !month || isNaN(month)) {
+			error = true;
+			innerDiv[1].classList.add("error");
+		}
+		if (day < 0 || day > DAYS_IN_MONTH[month] || !day || isNaN(day)) {
+			error = true;
+			innerDiv[2].classList.add("error");
+		}
+		if (error) { return }
+		url = location.href + "?n=" + name + "&m=" + month + "&d=" + day;
+		navigator.clipboard.writeText(url);
+	})
 }
